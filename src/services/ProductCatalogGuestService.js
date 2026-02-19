6ca7b1b4f5b57a5a30cb98f1ca43b4134da62811
Svit69 <ol.svitenkov@gmail.com>
2026-02-19 18:03:27 +0500

    chore: format catalog prices with thousands separators

--- src/services/ProductCatalogGuestService.js
+++ src/services/ProductCatalogGuestService.js
@@ -10,7 +10,7 @@
   buildGuestCatalogPayload() {
     const items = this.#repository.getAllProducts();
     if (!items.length) return { emptyMessage: 'Сейчас товаров нет. Скоро добавим карточки мерча.' };
-    const positions = items.map((item, i) => `${i + 1}. ${this.#formatter.formatBold(item.title)}\n${item.description}\nЦена: ${this.#formatter.formatBold(`от ${item.priceFrom} ₽`)}\nОсталось наборов: ${this.#formatter.formatBold(item.quantity)}`);
+    const positions = items.map((item, i) => `${i + 1}. ${this.#formatter.formatBold(item.title)}\n${item.description}\nЦена: ${this.#formatter.formatBold(`от ${new Intl.NumberFormat('ru-RU').format(item.priceFrom)} ₽`)}\nОсталось наборов: ${this.#formatter.formatBold(item.quantity)}`);
     const donationText = `${items.length + 1}. Не хочу ничего покупать, хочу просто пожертвовать деньги на благотворительность, <a href="https://help-children.net/campaign/obnovlenie-tehnologicheskoj-bazy-laboratorii-tsentra-detskoj-onkologii-i-gematologii-odkb-sleduyushhij-etap/">перейти на сайт фонда</a>.`;
     const notice = '<i>* количество товаров ограничено</i>';
     const text = [notice, ...positions, donationText].join('\n\n');
