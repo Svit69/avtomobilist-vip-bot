27cf4df620b0789371e2ea06a2a4c002c3f20d1c
Svit69 <ol.svitenkov@gmail.com>
2026-02-19 17:32:12 +0500

    chore: move donation link into merch post text

--- src/services/ProductCatalogGuestService.js
+++ src/services/ProductCatalogGuestService.js
@@ -11,12 +11,11 @@
     const items = this.#repository.getAllProducts();
     if (!items.length) return { emptyMessage: 'Сейчас товаров нет. Скоро добавим карточки мерча.' };
     const positions = items.map((item, i) => `${i + 1}. ${this.#formatter.formatBold(item.title)}\nЦена: ${this.#formatter.formatBold(`от ${item.priceFrom} ₽`)}`);
-    const donationText = `${items.length + 1}. Не хочу ничего покупать, хочу просто пожертвовать деньги на благотворительность.`;
+    const donationText = `${items.length + 1}. Не хочу ничего покупать, хочу просто пожертвовать деньги на благотворительность, <a href="https://help-children.net/campaign/obnovlenie-tehnologicheskoj-bazy-laboratorii-tsentra-detskoj-onkologii-i-gematologii-odkb-sleduyushhij-etap/">перейти на сайт фонда</a>.`;
     const text = [...positions, donationText].join('\n\n');
     const photos = items.flatMap(item => item.photos || []);
-    const cartButtons = items.map(item => [{ text: `${item.title}: добавить в корзину`, callback_data: `add_to_cart:${item.id}` }]);
-    const donationButton = [{ text: 'перейти на сайт фонда', url: 'https://help-children.net/campaign/obnovlenie-tehnologicheskoj-bazy-laboratorii-tsentra-detskoj-onkologii-i-gematologii-odkb-sleduyushhij-etap/' }];
-    return { text, photos, buttons: [...cartButtons, donationButton] };
+    const buttons = items.map(item => [{ text: `${item.title}: добавить в корзину`, callback_data: `add_to_cart:${item.id}` }]);
+    return { text, photos, buttons };
   }
 }
 
