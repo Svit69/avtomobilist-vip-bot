f6abb47f6e53afe9fc18a3b0ff9bcf974ab293d5
Svit69 <ol.svitenkov@gmail.com>
2026-02-20 09:38:15 +0500

    chore: update limited-stock notice wording

--- src/services/ProductCatalogGuestService.js
+++ src/services/ProductCatalogGuestService.js
@@ -12,7 +12,7 @@
     if (!items.length) return { emptyMessage: 'Сейчас товаров нет. Скоро добавим карточки мерча.' };
     const positions = items.map((item, i) => `${i + 1}. ${this.#formatter.formatBold(item.title)}\n${item.description}\nЦена: ${this.#formatter.formatBold(`от ${new Intl.NumberFormat('ru-RU').format(item.priceFrom)} ₽`)}\nОсталось наборов: ${this.#formatter.formatBold(`${item.quantity} шт`)}`);
     const donationText = `${items.length + 1}. Не хочу ничего покупать, хочу просто пожертвовать деньги на благотворительность, <a href="https://help-children.net/campaign/obnovlenie-tehnologicheskoj-bazy-laboratorii-tsentra-detskoj-onkologii-i-gematologii-odkb-sleduyushhij-etap/">перейти на сайт фонда</a>.`;
-    const notice = '<i>* количество товаров ограничено</i>';
+    const notice = '<i>* количество наборов ограничено</i>';
     const customOrderText = 'Для индивидуального заказа отдельных товаров у нас работает точка продаж на первом этаже в секторе А.';
     const text = [notice, ...positions, donationText, customOrderText].join('\n\n');
     const photos = items.flatMap(item => item.photos || []);
